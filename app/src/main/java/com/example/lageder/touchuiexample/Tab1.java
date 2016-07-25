9403c0e38e585d3d046d0855fd057b1a48703e42
Lageder <rageder01@gmail.com>
2016-07-25 16:17:42 +0900

    add animation on ProgressBar

--- app/src/main/java/com/example/lageder/touchuiexample/Tab1.java
+++ app/src/main/java/com/example/lageder/touchuiexample/Tab1.java
@@ -15,26 +15,64 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.lylc.widget.circularprogressbar.CircularProgressBar;
+
 /**
  * Created by Ferdousur Rahman Shajib on 27-10-2015.
  */
 public class Tab1 extends Fragment {
     Activity activity;
     Button button1;
+    CircularProgressBar c1;
+    int current_value;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.tab_1, container, false);
 
         activity = getActivity();
+        c1 = (CircularProgressBar) v.findViewById(R.id.circularprogressbar1);
+        c1.setSubTitle("작작 먹어라");
+        current_value = 95;
+        c1.animateProgressTo(0, current_value, new CircularProgressBar.ProgressAnimationListener() {
+            @Override
+            public void onAnimationStart() {
+            }
+
+            @Override
+            public void onAnimationFinish() {
+                c1.setTitle("돼지네 임마");
+            }
+
+            @Override
+            public void onAnimationProgress(int progress) {
+                c1.setTitle(progress + "%");
+            }
+        });
+
         button1 = (Button) v.findViewById(R.id.buttonTab1);
-        button1.setText("www.androstock.com");
+        button1.setText("RESET");
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.androstock.com"));
-                startActivity(browserIntent);
+                c1.animateProgressTo(current_value, 0, new CircularProgressBar.ProgressAnimationListener() {
+                    @Override
+                    public void onAnimationStart() {
+                    }
+
+                    @Override
+                    public void onAnimationFinish() {
+                        c1.setTitle("초기화");
+                        current_value = 0;
+                    }
+
+                    @Override
+                    public void onAnimationProgress(int progress) {
+                        c1.setTitle(progress + "%");
+                    }
+                });
             }
         });
 
